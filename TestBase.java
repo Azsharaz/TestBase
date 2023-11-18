1a3c833d8e6a1db061b088bc982f1533c897796a
Selen <selenoz97@gmail.com>
2023-11-18 23:46:07 +0300

    V1

---
+++ TestBase.java
@@ -0,0 +1,87 @@
+package utilities;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class TestBase {
+
+    protected WebDriver driver;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        //  driver.quit();
+    }
+
+    //Select Visible Text DropDown
+    public void selectVisible(WebElement ddm, String option){
+        Select select = new Select(ddm);
+        select.selectByVisibleText(option);
+    }
+
+    //Select index DropDown
+    public void selectIndex(WebElement ddm, int index){
+        Select select = new Select(ddm);
+        select.selectByIndex(index);
+    }
+
+    //Hard Wait
+    public void waitForSecond(int second)  {
+        try {
+            Thread.sleep(second*1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    //screenshot
+    public void screenShot(){
+        String date = DateTimeFormatter.ofPattern("ddMMyyyy_HHmmss").format( LocalDateTime.now() );
+        String dosyaYolu="src\\test\\java\\screenShots\\"+date+"screenShot.png";
+        TakesScreenshot ts = (TakesScreenshot) driver;
+        try {
+            Files.write(Paths.get(dosyaYolu), ts.getScreenshotAs(OutputType.BYTES));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //webelement screenshot
+    public void screenShotOfWebElement(WebElement webElement){
+
+        String date = DateTimeFormatter.ofPattern("ddMMyyyy_HHmmss").format( LocalDateTime.now() );
+
+        String dosyaYolu="src\\test\\java\\screenShots\\"+date+"webElementSS.png";
+
+        try {
+            Files.write(  Paths.get(dosyaYolu) , webElement.getScreenshotAs(OutputType.BYTES) );
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+
+}
